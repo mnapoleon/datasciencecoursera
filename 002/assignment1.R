cbebbbd4fdcfadf11a987d6b6487e371fa06c0df
mnapoleon <michael.napoleon@gmail.com>
2014-11-12 15:39:41 -0500

    more changes to complete function

--- 002/assignment1.R
+++ 002/assignment1.R
@@ -6,3 +6,13 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   }
   colMeans(data[pollutant], na.rm=TRUE)
 }
+
+complete <- function(directory, id = 1:332) {
+  all_files <- list.files(directory, full.names=TRUE)
+  data <- data.frame()
+  for (i in id) {
+    data <- rbind(data, read.csv(all_files[i]))
+  }
+  collist <- c('sulfate', 'nitrate')
+  nrow(data[complete.cases(data[collist]), collist])
+}
